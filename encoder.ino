af355f7a185c68092bad4585c299101631f9f3a6
YannSovanvichea <yannsovanvichea@gmail.com>
2021-02-18 22:40:55 +0700

    first commit

---
+++ encoder.ino
@@ -0,0 +1,39 @@
+
+ 
+ #define outputA 6
+ #define outputB 7
+
+ int counter = 0; 
+ int aState;
+ int aLastState;  
+
+ void setup() { 
+   pinMode (outputA,INPUT);
+   pinMode (outputB,INPUT);
+   
+   Serial.begin (9600);
+   
+   aLastState = digitalRead(outputA);   
+ } 
+
+ void loop() { 
+   aState = digitalRead(outputA); 
+   
+   if (aState != aLastState)
+   {     
+     if (digitalRead(outputB) != aState)
+     { 
+       counter ++;
+     } 
+     else 
+     {
+       counter --;
+     }
+     
+     Serial.print("Position: ");
+     Serial.println(counter);
+
+     
+   } 
+   aLastState = aState; 
+ }
